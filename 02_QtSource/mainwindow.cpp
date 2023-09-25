05ff0406d05c7be24f29aa8aec55faafaca163f9
MZWDAN <ouxue0303@163.com>
2023-09-25 21:52:56 +0800

    Add Qt Sources

---
+++ 02_QtSource/mainwindow.cpp
@@ -0,0 +1,25 @@
+﻿#include "mainwindow.h"
+#include <QMenu>
+#include <QMenuBar>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+    //菜单栏创建
+    QMenuBar *bar = menuBar();
+
+    //创建菜单成员
+    QMenu *fileMenu = bar->addMenu("File");
+    fileMenu->setIcon(QIcon("C:/Users/OUXUE/Desktop/ouxue.jpg"));
+//    QMenu *editMenu = bar->addMenu("Edit");
+
+    //使用添加Qt资源
+    //使用Qt的资源 ": + 前缀名 + 文件名"
+    fileMenu->setIcon(QIcon(":/images/ouxue.jpg"));
+
+}
+
+MainWindow::~MainWindow()
+{
+}
+
