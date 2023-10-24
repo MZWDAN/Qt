0c348570f38b62eed5aba98546dddab445e66704
MZWDAN <ouxue0303@163.com>
2023-10-24 23:46:46 +0800

    add QTreeWidgetItem

---
+++ 06_TreeWidget/mainwindow.cpp
@@ -0,0 +1,22 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QStringList>
+#include <QTreeWidgetItem>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->treeWidget->setHeaderLabels(QStringList()<<QString::fromLocal8Bit("英雄")
+                                    <<QString::fromLocal8Bit("英雄介绍"));
+
+    QTreeWidgetItem *item = new QTreeWidgetItem();
+    ui->treeWidget->addTopLevelItem(item);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
