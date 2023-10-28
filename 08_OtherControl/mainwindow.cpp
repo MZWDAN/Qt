5e80d936ea7e17ff25cad7c78bc77d19dce02ea9
MZWDAN <ouxue0303@163.com>
2023-10-28 22:52:34 +0800

    OtherControl

---
+++ 08_OtherControl/mainwindow.cpp
@@ -0,0 +1,18 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    //对栈容器进行修改
+//    connect(ui-)
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
