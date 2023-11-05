40a4a3f674576eb7ab3382b2fc4edfbc91d47ebb
MZWDAN <ouxue0303@163.com>
2023-11-05 23:36:00 +0800

    QEvent-V2

--- 10_QtEvent/mylabel.cpp
+++ 10_QtEvent/mylabel.cpp
@@ -4,7 +4,8 @@
 
 myLabel::myLabel(QWidget *parent) : QLabel(parent)
 {
-
+    //设置鼠标追踪
+    setMouseTracking(true); //默认是false
 }
 
 //鼠标进入事件
@@ -23,19 +24,26 @@ void myLabel::leaveEvent(QEvent *)
 //鼠标的移动
 void myLabel::mouseMoveEvent(QMouseEvent *ev)
 {
-    //这种写法有点链式编程的意思  %1 %2只是顺序
-    QString str = QString(QString::fromLocal8Bit("鼠标移动了,x=%1  y=%2 global_x=%3 global_y=%4"))
-            .arg(ev->x()).arg(ev->y()).arg(ev->globalX()).arg(ev->globalY());
-    qDebug() << str;
+//    if(ev->buttons() & Qt::LeftButton)
+//    {
+        //这种写法有点链式编程的意思  %1 %2只是顺序
+        QString str = QString(QString::fromLocal8Bit("鼠标移动了,x=%1  y=%2 global_x=%3 global_y=%4"))
+                .arg(ev->x()).arg(ev->y()).arg(ev->globalX()).arg(ev->globalY());
+        qDebug() << str;
+//    }
 }
 
 //鼠标的按下
 void myLabel::mousePressEvent(QMouseEvent *ev)
 {
-    //这种写法有点链式编程的意思  %1 %2只是顺序
-    QString str = QString(QString::fromLocal8Bit("鼠标按下了,x=%1  y=%2 global_x=%3 global_y=%4"))
-            .arg(ev->x()).arg(ev->y()).arg(ev->globalX()).arg(ev->globalY());
-    qDebug() << str;
+    //当鼠标左键按下，提示信息
+    if(ev->button() == Qt::LeftButton) //这个不会区分出来移动的状态，比如左右键都按下了，还是会响应
+    {
+        //这种写法有点链式编程的意思  %1 %2只是顺序
+        QString str = QString(QString::fromLocal8Bit("鼠标按下了,x=%1  y=%2 global_x=%3 global_y=%4"))
+                .arg(ev->x()).arg(ev->y()).arg(ev->globalX()).arg(ev->globalY());
+        qDebug() << str;
+    }
 }
 
 //鼠标的释放
