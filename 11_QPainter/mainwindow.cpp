821f7e8f2d6ca9750290a175a12ff90f6f879d08
MZWDAN <ouxue0303@163.com>
2023-11-14 23:17:12 +0800

    QPainter高级设置

--- 11_QPainter/mainwindow.cpp
+++ 11_QPainter/mainwindow.cpp
@@ -16,31 +16,56 @@ MainWindow::~MainWindow()
 
 void MainWindow::paintEvent(QPaintEvent *)
 {
-    //实例化画家对象  this指定的是绘图的设备
+//    //实例化画家对象  this指定的是绘图的设备
+//    QPainter painter(this);
+
+//    //设置画笔
+//    QPen pen(QColor(255, 100, 200));
+//    pen.setWidth(5);
+//    pen.setStyle(Qt::DashLine);
+
+//    //让画家使用这个笔
+//    painter.setPen(pen);
+
+//    //设置画刷
+//    QBrush brush(QColor(100, 100, 200));
+//    brush.setStyle(Qt::Dense1Pattern);
+//    painter.setBrush(brush);
+
+//    //画线
+//    painter.drawLine(QPoint(0, 0), QPoint(100, 100));
+
+//    //画圆
+//    painter.drawEllipse(QPoint(100, 100), 100, 50);
+
+//    //画矩形
+//    painter.drawRect(QRect(100, 100, 300, 300));
+
+//    //画文字
+//    painter.drawText(QRect(500, 500, 500, 500), QString::fromLocal8Bit("蛋蛋喜欢呆呆"));
+
+
+    ///////////////////////////////高级设置////////////////////////////////
     QPainter painter(this);
+//    painter.drawEllipse(QPoint(100, 100), 100, 100);
+//    //设置 抗锯齿能力
+//    //QPainter::Antialiasing: 效率较低
+//    painter.setRenderHint(QPainter::Antialiasing);
+//    painter.drawEllipse(QPoint(400, 100), 100, 100);
+    painter.drawRect(QRect(20, 20, 50, 50));
 
-    //设置画笔
-    QPen pen(QColor(255, 100, 200));
-    pen.setWidth(5);
-    pen.setStyle(Qt::DashLine);
+    //移动画家
+    painter.translate(100, 100);//相当于让画家移了一个位置
 
-    //让画家使用这个笔
-    painter.setPen(pen);
+    //保存画家状态
+    painter.save();
 
-    //设置画刷
-    QBrush brush(QColor(100, 100, 200));
-    brush.setStyle(Qt::Dense1Pattern);
-    painter.setBrush(brush);
+    painter.drawRect(QRect(20, 20, 50, 50));
 
-    //画线
-    painter.drawLine(QPoint(0, 0), QPoint(100, 100));
+    painter.translate(100, 100);
 
-    //画圆
-    painter.drawEllipse(QPoint(100, 100), 100, 50);
+    //还原画家保存状态
+    painter.restore();
 
-    //画矩形
-    painter.drawRect(QRect(100, 100, 300, 300));
-
-    //画文字
-    painter.drawText(QRect(500, 500, 500, 500), QString::fromLocal8Bit("蛋蛋喜欢呆呆"));
+    painter.drawRect(QRect(20, 20, 50, 50));
 }
