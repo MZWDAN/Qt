f5dec2bb0cb42599890244580d8e4e8bbf0f51f6
MZWDAN <ouxue0303@163.com>
2023-11-20 19:07:42 +0800

    QPixmap

---
+++ 12_QPainterDevice/widget.cpp
@@ -0,0 +1,33 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+#include <QPixmap>
+#include <QPainter>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    //Pixmap绘图设备
+    QPixmap pix(300, 300);
+
+    //填充颜色
+    pix.fill(QColor(Qt::white));
+
+    //声明画家
+    QPainter painter(&pix);
+    //设置颜色
+    painter.setPen(QPen(Qt::green));
+    painter.drawEllipse(QPoint(150, 150), 100, 100);
+
+    //保存
+    pix.save("save.png");
+
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
